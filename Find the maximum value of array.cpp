a68130aeff023823afde2bdc274ac5d368d3357b
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-08-21 19:33:34 +0530

    Find the maximum value of array

---
+++ Find the maximum value of array.cpp
@@ -0,0 +1,15 @@
+//Find the maximum value out of all the elements in the given array
+#include <iostream>
+using namespace std;
+int main(){
+	int arr[]={1,3,7,9,5};
+	int n= sizeof(arr)/4;
+	int mx= arr[0];
+
+	for(int i=0;i<n;i++){
+		if (arr[i]>mx) {
+		mx=arr[i];
+	}
+	}
+	cout<<"Maximum value is : "<<mx;
+}

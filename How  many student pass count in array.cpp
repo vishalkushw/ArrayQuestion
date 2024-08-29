3e3ac8d4ca3114a448ca2a276253f554e6ee2690
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-08-29 12:08:23 +0530

    How many student pass count in array

---
+++ How  many student pass count in array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int Marks[]={88,45,66,77,8,9,69};
+	int count=0;
+	int size=sizeof(Marks)/sizeof(Marks[0]);
+	for(int i=0;i<size;i++){
+		if(Marks[i]>=60){
+			count++;
+		}
+	}
+	cout<<count;
+	
+}

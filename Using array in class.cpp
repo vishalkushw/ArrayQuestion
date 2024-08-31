060991eb296a7ff84722ea8db0a7fbdbed400545
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-08-31 21:38:43 +0530

    Using array in class

---
+++ Using array in class.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+	
+class Shop{
+	private:
+	int itemID[100];
+	int itemPrice[100];
+	int counter;
+	public:
+		void initCounter(void){
+		counter=0;
+		}
+		void setprice(void){
+			cout<<"Enter ID of your item no "<<counter+1<<endl;
+			cin>>itemID[counter];
+			cout<<"Enter price of your item "<<endl;
+			cin>>itemPrice[counter];
+			counter++;
+		} 
+		
+		void displayPrice(void){
+			for(int i=0;i<counter;i++){
+			cout<<"The Price of item with ID"<<itemID[i]<<" is ";
+			cout<<itemPrice[i]<<endl;
+			}
+		}
+	
+	
+};	
+int main(){
+	Shop sp;
+	sp.initCounter();
+	sp.setprice();
+	sp.setprice();
+	sp.setprice();
+	sp.setprice();
+	sp.displayPrice();
+}

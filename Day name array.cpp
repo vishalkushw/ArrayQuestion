5387f53ea1c01b5c434aeca9b8e4429ea68755f5
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-24 18:09:43 +0530

    Day name array

---
+++ Day name array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string arr[7]={"Sunday","Monday","Tuesday","Wednesday","Thrusday"
+	               ,"Friday","Saturday"};
+	
+	for (int i=0;i<7;i++){
+		cout<<arr[i]<<endl;
+	}
+}

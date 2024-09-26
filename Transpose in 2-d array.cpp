de431aea5c8bbefb8f07abebafeaac1956b46cb0
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-26 20:42:00 +0530

    Transpose in 2-d array

---
+++ Transpose in 2-d array.cpp
@@ -0,0 +1,20 @@
+// WAP to transpose in 2-d array row into column and column into row
+
+
+#include<iostream>
+using namespace std;
+int main(){ 
+    int arr1[4][3]={1,1,2,5,6,7,5,5,5,4,6,7};
+    int arr2[3][4];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            arr2[i][j]=arr1[j][i];
+        }
+    }
+      for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            cout<<arr2[i][j]<<" ";
+}
+cout<<endl;
+    }
+}

f7c8d9b45077e04c3abe656ef45b59415c2ae0eb
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-27 20:44:46 +0530

    How many time Arrary inter

---
+++ Array Question/How many time Arrary inter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int arr[5];
+	cout<<"Enter 5 value ";
+	for (int i=0;i<5;i=i+2){
+		cout<<"Enter value of index number "<<i<<" ";
+		cin>>arr[i];
+	}
+	cout<<"Data store in array"<<endl; 
+	for (int i=0;i<5;i++){
+		cout<<arr[i]<<" ";
+	}
+	
+}
+
+

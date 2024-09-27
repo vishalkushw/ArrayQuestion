cec080ce1bbd6433d26d8fe260788ed195bcdc30
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-27 20:43:14 +0530

    array store value

---
+++ Array Question/array store value.cpp
@@ -0,0 +1,8 @@
+ // array store value
+#include <iostream>
+using namespace std;
+int main(){
+	int arr[5]={22,41,21,45,67};
+	cout<<"max value"<<INT_MAX <<endl;//2147483647
+	cout<<"min value"<<INT_MIN;  //2147483648	
+}

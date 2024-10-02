4924da4e7412a7ce67aad14fe1a970e596f1156d
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-10-02 12:04:40 +0530

    Replace 0 to -1 array

---
+++ Array Question/Replace 0 to -1 array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int arr[5];
+	int min=arr[0];
+	cout<<"Enter 5 value ";
+	for (int i=0;i<5;i++){
+		cout<<"Enter value of index number "<<i<<" ";
+		cin>>arr[i];
+	}
+	for(int i=0;i<5;i++){
+		if (arr[i]==0) 
+		arr[i] = -1;
+	    cout<<arr[i] <<" ";
+	}	
+}

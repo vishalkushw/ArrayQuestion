bfc355b7c5747d34305af6a0ae9058141b78cb75
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-10-22 12:46:25 +0530

    Square function

---
+++ Array Question/Square function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void sqr(int n){
+	
+	 cout<<"Number square is "<<n*n;
+	
+}
+
+int main(){
+	int n;
+	cout<<"Enter number : ";
+	cin>>n;
+   
+   sqr(n);
+	
+}
